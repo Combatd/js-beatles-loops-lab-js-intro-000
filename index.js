82ae9bdb26ec897d152344858fc3ffe768295fbe
Mark Calvelo <combatdstar@gmail.com>
2019-08-28 12:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function iLoveTheBeatles(number) { // takes a number as a parameter
   do {
     love_arr[i] = "I love the Beatles!";
     i++ // increment the index
-  } while (i < number) // run it number amounts of times
+  } while (i === number) // run it number amounts of times
   return love_arr; // return the array
 }
